076b9fb817cc4cf0e8fb01dd0b1f9b3fb426e98f
Arsen Klinichev <arsenklinichev@gmail.com>
2016-11-13 21:20:32 +0300

    Change the structure of the project, make it multi module

---
+++ maintest/src/main/java/ru/klinichev/Main.java
@@ -0,0 +1,12 @@
+package ru.klinichev;
+
+import com.math.MyMath;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyMath math = new MyMath();
+		System.out.println(math.add(9, 15));
+	}
+
+}
---
+++ mathtest/src/main/java/com/math/MyMath.java
@@ -0,0 +1,10 @@
+package com.math;
+
+public class MyMath {
+	
+	public String add (int a, int b) {
+		int c = a + b;
+		return "The sum of " + a + " and " + b + " is " + c;
+	}
+
+}
